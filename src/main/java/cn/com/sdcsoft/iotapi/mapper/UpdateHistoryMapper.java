05d0f35d945cd4ba71116f7f8380019c23738605
dusen <1005735462@qq.com>
2019-10-23 14:40:13 +0800

    添加查询条件

--- src/main/java/cn/com/sdcsoft/iotapi/mapper/UpdateHistoryMapper.java
+++ src/main/java/cn/com/sdcsoft/iotapi/mapper/UpdateHistoryMapper.java
@@ -16,6 +16,12 @@ public interface UpdateHistoryMapper {
             "<if test='simNo!= null and simNo.length>0 '> " +
             "AND SimNo = #{simNo}"+
             "</if>" +
+            "<if test='RawState!= null and rawState.length>0 '> " +
+            "AND RawState = #{rawState}"+
+            "</if>" +
+            "<if test='NowState!= null and nowState.length>0 '> " +
+            "AND NowState = #{nowState}"+
+            "</if>" +
             "<if test='type!= 0  '> " +
             "AND Type = #{type}"+
             "</if>" +
