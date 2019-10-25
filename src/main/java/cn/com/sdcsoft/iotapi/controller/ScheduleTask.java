e8649a590d101227526f9d4c96cbc0b8e6095caf
dusen <1005735462@qq.com>
2019-10-25 11:40:09 +0800

    添加不间断扫描

--- src/main/java/cn/com/sdcsoft/iotapi/controller/ScheduleTask.java
+++ src/main/java/cn/com/sdcsoft/iotapi/controller/ScheduleTask.java
@@ -10,16 +10,22 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 
-@RestController
-@RequestMapping(value = "/check")
-public class CheckController {
+@Component
+@Configuration
+@EnableScheduling
+public class ScheduleTask {
 
     @Autowired
     private SimMapper simMapper;
@@ -30,13 +36,16 @@ public class CheckController {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @GetMapping("/find")
+    //3.添加定时任务
+    //@Scheduled(cron = "0/5 * * * * ?")
+    //或直接指定时间间隔，例如：5秒
+    @Scheduled(fixedDelay=1800000)
     public void check() throws IOException {
-
+        System.out.println(new Date());
         List<Sim> simList=  simMapper.findSim(new Sim());
         for(int i=0;i<simList.size();i++){
-            String simSql="";
-            String updateSql="";
+            String simDate="";
+            String updateDate="";
             Request cookieRequest = new Request.Builder()
                     .url("https://api.iot.10086.cn/v2/querycardlifecycle?appid=QHSLKYFFDM4TJO&transid=QHSLKYFFDM4TJO2019101410533080000001&ebid=0001000000432&token=3740d7d73ea1e228d08b5045434c09248e0883eb7d5ac0576e69e6fe2f6acd01&iccid="+simList.get(i).getSimNo())
                     .get()
@@ -46,10 +55,12 @@ public class CheckController {
             execute.close();
             JSONArray result1 = JSONObject.parseArray(result.get("result").toString());
 
+
             if(result.get("status").equals("0")&!result1.getJSONObject(0).get("lifecycle").equals(simList.get(i).getLifecycle())){
-                simSql+="update Sim set Lifecycle="+result1.getJSONObject(0).get("lifecycle");
-                updateSql="insert into Updatehistory (SimNo,RawState,NowState,CreateDatetime,Type) values ( '"+simList.get(i).getSimNo()+"' ,"+simList.get(i).getLifecycle()+","+result1.getJSONObject(0).get("lifecycle")+",now(),1)";
-                rabbitTemplate.convertAndSend(updateSql);
+
+                simDate="update_"+simList.get(i).getId()+"_Lifecycle_"+result1.getJSONObject(0).get("lifecycle");
+                updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getLifecycle()+"_"+result1.getJSONObject(0).get("lifecycle")+"_1";
+                 rabbitTemplate.convertAndSend(updateDate);
                  cookieRequest = new Request.Builder()
                         .url("https://api.iot.10086.cn/v2/gprsrealsingle?appid=QHSLKYFFDM4TJO&transid=QHSLKYFFDM4TJO2019101410533080000001&ebid=0001000000008&token=3740d7d73ea1e228d08b5045434c09248e0883eb7d5ac0576e69e6fe2f6acd01&iccid="+simList.get(i).getSimNo())
                         .get()
@@ -61,15 +72,15 @@ public class CheckController {
                 if(result2.get("status").equals("0")&!result3.getJSONObject(0).get("GPRSSTATUS").equals(simList.get(i).getState())){
 
                     if(result3.getJSONObject(0).get("GPRSSTATUS").equals("")){
-                        simSql+=",State=00";
-                        updateSql="insert into Updatehistory (SimNo,RawState,NowState,CreateDatetime,Type) values ( '"+simList.get(i).getSimNo()+"' ,"+simList.get(i).getState()+",00,now(),2)";
+                        simDate+="_State_00";
+                        //updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getState()+"_00_2";
+
                     }else{
-                        simSql+=",State="+result3.getJSONObject(0).get("GPRSSTATUS");
-                        updateSql="insert into Updatehistory (SimNo,RawState,NowState,CreateDatetime,Type) values ( '"+simList.get(i).getSimNo()+"' ,"+simList.get(i).getState()+","+result3.getJSONObject(0).get("GPRSSTATUS")+",now(),2)";
+                        simDate+="_State_"+result3.getJSONObject(0).get("GPRSSTATUS");
+                        updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getState()+"_"+result3.getJSONObject(0).get("GPRSSTATUS")+"_2";
+                        rabbitTemplate.convertAndSend(updateDate);
                     }
-                    simSql+=" where Id="+simList.get(i).getId();
-                    rabbitTemplate.convertAndSend(updateSql);
-                    rabbitTemplate.convertAndSend(simSql);
+                    rabbitTemplate.convertAndSend(simDate);
                 }
             }
 
