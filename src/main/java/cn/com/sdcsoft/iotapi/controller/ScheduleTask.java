c1cfa33c59230a5501a2179a518d0be737702ebd
dusen <1005735462@qq.com>
2019-10-28 11:34:50 +0800

    修改bug

--- src/main/java/cn/com/sdcsoft/iotapi/controller/ScheduleTask.java
+++ src/main/java/cn/com/sdcsoft/iotapi/controller/ScheduleTask.java
@@ -56,31 +56,34 @@ public class ScheduleTask {
             JSONArray result1 = JSONObject.parseArray(result.get("result").toString());
 
 
-            if(result.get("status").equals("0")&!result1.getJSONObject(0).get("lifecycle").equals(simList.get(i).getLifecycle())){
+            if(result.get("status").equals("0")){
+                if(!result1.getJSONObject(0).get("lifecycle").equals(simList.get(i).getLifecycle())){
+                    simDate="update_"+simList.get(i).getId()+"_Lifecycle_"+result1.getJSONObject(0).get("lifecycle");
+                    updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getLifecycle()+"_"+result1.getJSONObject(0).get("lifecycle")+"_1";
+                    rabbitTemplate.convertAndSend(updateDate);
+                    cookieRequest = new Request.Builder()
+                            .url("https://api.iot.10086.cn/v2/gprsrealsingle?appid=QHSLKYFFDM4TJO&transid=QHSLKYFFDM4TJO2019101410533080000001&ebid=0001000000008&token=3740d7d73ea1e228d08b5045434c09248e0883eb7d5ac0576e69e6fe2f6acd01&iccid="+simList.get(i).getSimNo())
+                            .get()
+                            .build();
+                    execute = okHttpClient.newCall(cookieRequest).execute();
+                    JSONObject result2 = JSONObject.parseObject(execute.body().string());
+                    JSONArray result3 = JSONObject.parseArray(result2.get("result").toString());
 
-                simDate="update_"+simList.get(i).getId()+"_Lifecycle_"+result1.getJSONObject(0).get("lifecycle");
-                updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getLifecycle()+"_"+result1.getJSONObject(0).get("lifecycle")+"_1";
-                 rabbitTemplate.convertAndSend(updateDate);
-                 cookieRequest = new Request.Builder()
-                        .url("https://api.iot.10086.cn/v2/gprsrealsingle?appid=QHSLKYFFDM4TJO&transid=QHSLKYFFDM4TJO2019101410533080000001&ebid=0001000000008&token=3740d7d73ea1e228d08b5045434c09248e0883eb7d5ac0576e69e6fe2f6acd01&iccid="+simList.get(i).getSimNo())
-                        .get()
-                        .build();
-                execute = okHttpClient.newCall(cookieRequest).execute();
-                JSONObject result2 = JSONObject.parseObject(execute.body().string());
-                JSONArray result3 = JSONObject.parseArray(result2.get("result").toString());
+                    if(result2.get("status").equals("0")){
+                        if(!result3.getJSONObject(0).get("GPRSSTATUS").equals(simList.get(i).getState())){
+                            if(result3.getJSONObject(0).get("GPRSSTATUS").equals("")){
+                                simDate+="_State_00";
+                                //updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getState()+"_00_2";
 
-                if(result2.get("status").equals("0")&!result3.getJSONObject(0).get("GPRSSTATUS").equals(simList.get(i).getState())){
+                            }else{
+                                simDate+="_State_"+result3.getJSONObject(0).get("GPRSSTATUS");
+                                updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getState()+"_"+result3.getJSONObject(0).get("GPRSSTATUS")+"_2";
+                                rabbitTemplate.convertAndSend(updateDate);
+                            }
+                            rabbitTemplate.convertAndSend(simDate);
+                        }
 
-                    if(result3.getJSONObject(0).get("GPRSSTATUS").equals("")){
-                        simDate+="_State_00";
-                        //updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getState()+"_00_2";
-
-                    }else{
-                        simDate+="_State_"+result3.getJSONObject(0).get("GPRSSTATUS");
-                        updateDate="insert_"+simList.get(i).getSimNo()+"_"+simList.get(i).getState()+"_"+result3.getJSONObject(0).get("GPRSSTATUS")+"_2";
-                        rabbitTemplate.convertAndSend(updateDate);
                     }
-                    rabbitTemplate.convertAndSend(simDate);
                 }
             }
 
